b9700521121b3bbd404c7064bb9d429176f1bcce
«Ales» <alopusansky@gmail.com>
2023-07-13 13:45:22 +0300

    initial commit

---
+++ task_001/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1 - Слушатель написал корректную программу с использованием рекурсии по выведению всех натуральных чисел в промежутке от N до 1.
+
+int ReadInt(string messege)
+{
+    System.Console.Write(messege);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintElements(int N)
+{
+    if (N == 1) return;
+}
+
---
+++ task_002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadInt(string messege)
+{
+    System.Console.Write(messege);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumElement(int m, int n)
+{
+    if (m == n) return n;
+    else return m + SumElement(m + 1, n);
+}
+
+
+int leftRange = ReadInt("Enter left number: ");
+int rightRange = ReadInt("Enter right number: ");
+
+
+
+System.Console.WriteLine(SumElement(leftRange, rightRange));
