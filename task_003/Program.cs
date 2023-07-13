47722531b212ea2ab1d61dcbc3c54776cb388362
«Ales» <alopusansky@gmail.com>
2023-07-13 15:45:29 +0300

    add solution it task_002

--- task_003/Program.cs
+++ task_003/Program.cs
@@ -3,11 +3,11 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 
-int AkkermanFunction(int n, int m)
+int AckermanFunction(int n, int m)
 {
     if (n == 0) return m + 1;
-    else if ((n != 0) && (m == 0)) return AkkermanFunction(n - 1, 1);
-    else return AkkermanFunction(n - 1, AkkermanFunction(n, m - 1));
+    else if ((n != 0) && (m == 0)) return AckermanFunction(n - 1, 1);
+    else return AckermanFunction(n - 1, AckermanFunction(n, m - 1));
 }
 
-System.Console.WriteLine(AkkermanFunction(2,2));
+System.Console.WriteLine(AckermanFunction(3,11));
